442685c89de94e8eea83568b8f0e071ab95d5db9
Markus Petzke <markuspetzke@gmail.com>
2026-03-01 14:36:30 +0100

    basic text rendering

--- src/rain.ts
+++ src/rain.ts
@@ -1,8 +1,33 @@
-const canvas = document.getElementById("canvas-rain");
-const h1 = document.createElement("h1");
+const canvas: HTMLCanvasElement = document.getElementById(
+  "canvas-rain",
+) as HTMLCanvasElement;
 
-h1.textContent = "test";
+const ctx = canvas.getContext("2d");
 
-canvas?.appendChild(h1);
+type COLUMN = string[];
+type MATRIX = COLUMN[];
+const CELL_SIZE = 32;
 
-console.log("");
+const matrix: MATRIX = [
+  ["a", "b", "c"],
+  ["1", "2", "3"],
+];
+
+if (ctx != null) {
+  ctx.font = "32px mono ";
+  ctx.fillStyle = "green";
+  // ctx.fillText("0", 10, 30);
+  render(matrix, ctx);
+}
+
+function render(matrix: MATRIX, ctx: CanvasRenderingContext2D) {
+  let x = CELL_SIZE;
+  for (const column of matrix) {
+    let y = CELL_SIZE;
+    for (const letter of column) {
+      ctx.fillText(letter, x, y);
+      y += CELL_SIZE;
+    }
+    x += CELL_SIZE;
+  }
+}
