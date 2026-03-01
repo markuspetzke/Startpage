322173dfa7e6bc76a8cf8b7362fb70c09bb2fde9
Markus Petzke <markuspetzke@gmail.com>
2026-03-01 15:31:43 +0100

    dynamic scaling

--- src/rain.ts
+++ src/rain.ts
@@ -7,21 +7,19 @@ const ctx = canvas.getContext("2d");
 type COLUMN = string[];
 type MATRIX = COLUMN[];
 
-const WIDTH = canvas.width;
-const HEIGHT = canvas.height;
+const WIDTH = canvas.clientWidth;
+const HEIGHT = canvas.clientHeight;
+canvas.height = HEIGHT;
+canvas.width = WIDTH;
 const TEXT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBER = "1234567890";
 const ALPHABET = TEXT + NUMBER;
 const CELL_SIZE = 32;
 
-const matrix: MATRIX = [
-  ["a", "b", "c"],
-  ["1", "2", "3"],
-];
-
 if (ctx != null) {
-  ctx.font = "32px mono ";
+  ctx.font = CELL_SIZE + "px mono ";
   ctx.fillStyle = "green";
+
   render(ctx);
 }
 
@@ -30,11 +28,11 @@ function randomIntFromInterval(min: number, max: number) {
 }
 
 function render(ctx: CanvasRenderingContext2D) {
-  const row_count = Math.ceil(HEIGHT / CELL_SIZE) * CELL_SIZE;
+  const row_count = Math.floor(HEIGHT / CELL_SIZE) * CELL_SIZE;
   const column_count = Math.floor(WIDTH / CELL_SIZE) * CELL_SIZE;
 
-  for (let x = 0; x < column_count; x += CELL_SIZE) {
-    for (let y = CELL_SIZE; y < row_count; y += CELL_SIZE) {
+  for (let x = 0; x <= column_count; x += CELL_SIZE) {
+    for (let y = CELL_SIZE; y <= row_count; y += CELL_SIZE) {
       const letter = ALPHABET.charAt(
         randomIntFromInterval(0, ALPHABET.length - 1),
       );
