db345630ab3634661c872b918165b8fedf6c4748
Markus Petzke <markuspetzke@gmail.com>
2026-03-01 15:45:27 +0100

    exclued createMatrix into function

--- src/rain.ts
+++ src/rain.ts
@@ -19,24 +19,44 @@ const CELL_SIZE = 32;
 if (ctx != null) {
   ctx.font = CELL_SIZE + "px mono ";
   ctx.fillStyle = "green";
+  let matrix = createMatrix();
 
-  render(ctx);
+  render(matrix, ctx);
+}
+
+function createMatrix(): MATRIX {
+  let matrix: MATRIX = [];
+  const row_count = Math.floor(HEIGHT / CELL_SIZE) * CELL_SIZE;
+  const column_count = Math.floor(WIDTH / CELL_SIZE) * CELL_SIZE;
+
+  for (let x = 0; x <= column_count; x += CELL_SIZE) {
+    let column: COLUMN = [];
+    for (let y = CELL_SIZE; y <= row_count; y += CELL_SIZE) {
+      const letter = ALPHABET.charAt(
+        randomIntFromInterval(0, ALPHABET.length - 1),
+      );
+      column.push(letter);
+    }
+    matrix.push(column);
+  }
+  console.log("width: " + matrix.length);
+  console.log("height: " + matrix[0].length);
+  return matrix;
 }
 
 function randomIntFromInterval(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function render(ctx: CanvasRenderingContext2D) {
-  const row_count = Math.floor(HEIGHT / CELL_SIZE) * CELL_SIZE;
-  const column_count = Math.floor(WIDTH / CELL_SIZE) * CELL_SIZE;
-
-  for (let x = 0; x <= column_count; x += CELL_SIZE) {
-    for (let y = CELL_SIZE; y <= row_count; y += CELL_SIZE) {
-      const letter = ALPHABET.charAt(
-        randomIntFromInterval(0, ALPHABET.length - 1),
-      );
-      ctx.fillText(letter, x, y);
+function render(matrix: MATRIX, ctx: CanvasRenderingContext2D) {
+  let x = 0;
+  for (const column of matrix) {
+    let y = CELL_SIZE;
+    for (const char of column) {
+      ctx.fillText(char, x, y);
+      y += CELL_SIZE;
     }
+
+    x += CELL_SIZE;
   }
 }
