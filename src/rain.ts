30c55b70a9e1522d0d45d86f83a45d200fbb1b04
Markus Petzke <markuspetzke@gmail.com>
2026-03-02 14:29:17 +0100

    fixed wrong matrix positioning

--- src/rain.ts
+++ src/rain.ts
@@ -4,7 +4,14 @@ const canvas: HTMLCanvasElement = document.getElementById(
 
 const ctx = canvas.getContext("2d");
 
-type COLUMN = string[];
+type Cell = {
+  position: number;
+  char: string;
+};
+type COLUMN = {
+  cells: Cell[];
+  head: Cell;
+};
 type MATRIX = COLUMN[];
 
 const WIDTH = canvas.clientWidth;
@@ -16,32 +23,60 @@ const NUMBER = "1234567890";
 const ALPHABET = TEXT + NUMBER;
 const CELL_SIZE = 32;
 
-if (ctx != null) {
+if (ctx) {
   ctx.font = CELL_SIZE + "px mono ";
   ctx.fillStyle = "green";
-  let matrix = createMatrix();
 
-  render(matrix, ctx);
+  let matrix = createMatrix();
+  window.setInterval(() => {
+    console.log("tick");
+
+    tick(matrix);
+    render(matrix, ctx);
+  }, 1000);
 }
 
 function createMatrix(): MATRIX {
   let matrix: MATRIX = [];
-  const row_count = Math.floor(HEIGHT / CELL_SIZE) * CELL_SIZE;
-  const column_count = Math.floor(WIDTH / CELL_SIZE) * CELL_SIZE;
 
-  for (let x = 0; x <= column_count; x += CELL_SIZE) {
-    let column: COLUMN = [];
-    for (let y = CELL_SIZE; y <= row_count; y += CELL_SIZE) {
+  const row_count = Math.floor(HEIGHT / CELL_SIZE);
+  const column_count = Math.floor(WIDTH / CELL_SIZE);
+
+  for (let x = 0; x <= column_count; x += 1) {
+    let cells: Cell[] = [];
+    for (let y = 0; y <= row_count; y += 1) {
       const letter = ALPHABET.charAt(
         randomIntFromInterval(0, ALPHABET.length - 1),
       );
-      column.push(letter);
+
+      const cell: Cell = {
+        position: y,
+        char: letter,
+      };
+      cells.push(cell);
     }
-    matrix.push(column);
+    matrix.push({ cells, head: cells[0] });
   }
-  console.log("width: " + matrix.length);
-  console.log("height: " + matrix[0].length);
   return matrix;
+}
+
+function tick(matrix: MATRIX) {
+  for (const column of matrix) {
+    const nextCell = column.cells[column.head.position + 1];
+
+    console.log(column.head.position + 1);
+
+    if (nextCell) {
+      column.head.char = "";
+      nextCell.char = ALPHABET.charAt(
+        randomIntFromInterval(0, ALPHABET.length - 1),
+      );
+      column.head = nextCell;
+    } else {
+      column.head.char = "";
+      column.head = column.cells[0];
+    }
+  }
 }
 
 function randomIntFromInterval(min: number, max: number) {
@@ -49,11 +84,14 @@ function randomIntFromInterval(min: number, max: number) {
 }
 
 function render(matrix: MATRIX, ctx: CanvasRenderingContext2D) {
+  ctx.fillStyle = "rgb(0,16,0)";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = "green";
   let x = 0;
   for (const column of matrix) {
     let y = CELL_SIZE;
-    for (const char of column) {
-      ctx.fillText(char, x, y);
+    for (const cell of column.cells) {
+      ctx.fillText(cell.char, x, y);
       y += CELL_SIZE;
     }
 
