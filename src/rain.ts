67dc41988d8d6017fc06a751bf06bb37acd6b56e
Markus Petzke <markuspetzke@gmail.com>
2026-03-02 15:41:59 +0100

    added dynamic resizing

--- src/rain.ts
+++ src/rain.ts
@@ -25,24 +25,30 @@ type Greens = (typeof GREENS)[number];
 
 type Color = typeof WHITE | Greens;
 
-const WIDTH = canvas.clientWidth;
-const HEIGHT = canvas.clientHeight;
-const RAINDROP_SWPAN_RATE = 0.8;
-canvas.height = HEIGHT;
-canvas.width = WIDTH;
+let width = canvas.clientWidth;
+let height = canvas.clientHeight;
+canvas.height = height;
+canvas.width = width;
 const TEXT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const NUMBER = "1234567890";
 const ALPHABET = TEXT + NUMBER;
-const CELL_SIZE = 32;
+let CELL_SIZE = Math.floor(width / 40);
 
-const ROW_COUNT = Math.floor(HEIGHT / CELL_SIZE);
-const COLUMN_COUNT = Math.floor(WIDTH / CELL_SIZE);
+const RAINDROP_SWPAN_RATE = 0.8;
+
+let row_count = Math.floor(height / CELL_SIZE);
+let column_count = Math.floor(width / CELL_SIZE);
+let matrix: MATRIX;
+
+ctx;
 
 if (ctx) {
   ctx.font = CELL_SIZE + "px mono ";
   ctx.fillStyle = "green";
 
-  let matrix = createMatrix();
+  window.addEventListener("resize", resizeCanvas);
+
+  matrix = createMatrix();
   window.setInterval(() => {
     tick(matrix);
     render(matrix, ctx);
@@ -51,9 +57,6 @@ if (ctx) {
 
 function createMatrix(): MATRIX {
   let matrix: MATRIX = [];
-
-  const row_count = Math.floor(HEIGHT / CELL_SIZE);
-  const column_count = Math.floor(WIDTH / CELL_SIZE);
 
   for (let x = 0; x < column_count; x++) {
     let cells: Cell[] = [];
@@ -75,7 +78,7 @@ function tick(matrix: MATRIX) {
   for (const column of matrix) {
     const animationComplete = column.head === undefined;
     if (animationComplete && Math.random() > RAINDROP_SWPAN_RATE) {
-      column.trail = randomIntFromInterval(3, 2 * ROW_COUNT);
+      column.trail = randomIntFromInterval(3, 2 * row_count);
       column.head = column.cells[0];
       column.head.char = randomChar();
     } else {
@@ -114,7 +117,7 @@ function randomIntFromInterval(min: number, max: number) {
 
 function render(matrix: MATRIX, ctx: CanvasRenderingContext2D) {
   ctx.fillStyle = "rgb(0,0,0)";
-  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillRect(0, 0, width, height);
   let x = 0;
   for (const column of matrix) {
     let y = 0;
@@ -125,4 +128,19 @@ function render(matrix: MATRIX, ctx: CanvasRenderingContext2D) {
     }
     x += CELL_SIZE;
   }
+}
+
+function resizeCanvas() {
+  width = canvas.clientWidth;
+  height = canvas.clientHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  row_count = Math.floor(height / CELL_SIZE);
+  column_count = Math.floor(width / CELL_SIZE);
+
+  CELL_SIZE = Math.floor(width / 40);
+
+  ctx!.font = CELL_SIZE + "px mono ";
+  matrix = createMatrix();
 }
