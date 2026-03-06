75e270012383c014aa2d0053031a9d46a96b48e9
Markus Petzke <markuspetzke@gmail.com>
2026-03-06 17:03:50 +0100

    added image avg color fn

--- src/image.ts
+++ src/image.ts
@@ -13,10 +13,44 @@ if (canvas_image) {
 
   if (ctx_image) {
     let image = new Image();
+
     image.onload = function () {
       ctx_image.drawImage(image, 0, 0, width, height);
+      get_avg_color(ctx_image.getImageData(0, 0, width, height));
     };
-
     image.src = startImage;
   }
+}
+
+function get_avg_color(image: ImageData) {
+  let i = -4;
+  let blockSize = 5;
+
+  let count = 0;
+  var rgb = { r: 0, g: 0, b: 0 };
+  let length = image.data.length;
+
+  while ((i += blockSize * 4) < length) {
+    count++;
+    rgb.r += image.data[i];
+    rgb.g += image.data[i + 1];
+    rgb.b += image.data[i + 2];
+  }
+  rgb.r = ~(rgb.r / count) * -1;
+  rgb.g = ~(rgb.g / count) * -1;
+  rgb.b = ~(rgb.b / count) * -1;
+
+  console.log("r: " + rgb.r);
+  console.log("g: " + rgb.g);
+  console.log("b: " + rgb.b);
+
+  let body = document.getElementById("body") as HTMLBodyElement;
+  body.style =
+    "background-color: RGB(" +
+    rgb.r +
+    "," +
+    rgb.g +
+    "," +
+    rgb.b +
+    "); display: flex";
 }
